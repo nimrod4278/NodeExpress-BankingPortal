61f071921b8e514f71f72cd8d1d4ded801b0982a
Nimrod Feldman <Nimrod.feldman@gmail.com>
2019-07-21 15:24:47 +0300

    Handling form data

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.set('view engine', 'ejs');
 // can only send static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({extended: true}));
+
 // reads acounts.json from json lib in utf8 format
 const accountData = fs.readFileSync(
     path.join(__dirname, 'json', 'accounts.json'), 'utf8'
@@ -53,5 +55,27 @@ app.get('/credit', (req, res) => {
 app.get('/profile', (req, res) => {
     res.render('profile', {user: users[0] } )
 });
+
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - 
+    req.body.amount;
+    accounts[req.body.to].balance = accounts[req.body.to].balance +
+    parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', { message: 'Tranfer Completed'});
+});
+
+app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit}));
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
+
 
 app.listen(3000, () => console.log('PS Project Running on port 3000'));
